37fe6b9bcf6fb7624aca17537228d01f3b954ba1
Rafael Ortiz <61321805+Rafael9902@users.noreply.github.com>
2024-10-30 19:38:22 -0500

    feat: add question layer instantiate

--- modules/question_point/question_point.gd
+++ modules/question_point/question_point.gd
@@ -5,6 +5,8 @@ signal question_entered(question_id)
 @export var level: int
 @export var number: int
 
+var question_layer = preload("res://modules/question_layer/question_layer.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -15,4 +17,7 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_body_entered(body: Node2D) -> void:
-	pass
+	var question_instance = question_layer.instantiate()
+	
+	add_child(question_instance)
+	print(question_instance)
