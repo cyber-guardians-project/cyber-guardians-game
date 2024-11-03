5e0c10a53921979faa2f91162e32b4307daa9ac3
Rafael Ortiz <61321805+Rafael9902@users.noreply.github.com>
2024-11-02 23:27:09 -0500

    fix: question layer

--- modules/question_layer/question_layer.gd
+++ modules/question_layer/question_layer.gd
@@ -2,16 +2,17 @@ extends Control
 
 @export var question = {}
 
-@onready var label = $Label
-@onready var question_options_control = $QuestionOptions  # This can be a regular Control node, not necessarily a container
+@onready var question_title = $Title
+@onready var question_options_control = $QuestionOptions
+@onready var checkbox_custom_theme = preload("res://themes/checkbox.tres")
 
-# Adjust this to control the vertical spacing between checkboxes
 var checkbox_spacing = 30
 
 func _ready() -> void:
 	if question:
 		var question_object = question[0]
-		label.text = question_object.question
+		question_title.text = question_object.question
+		question_title.autowrap_mode = true
 		print(question_object.question)
 		
 		clear_children()
@@ -22,17 +23,16 @@ func _ready() -> void:
 
 func create_option_checkbox(option: Dictionary, index: int) -> void:
 	var checkbox = CheckBox.new()
+	
 	checkbox.text = option["text"]
 	checkbox.set_meta("is_correct", option["is_correct"])
-	
-	# Set position manually based on index and spacing
 	checkbox.position = Vector2(0, index * checkbox_spacing)
-	
-	checkbox.add_theme_font_size_override('font', 8)
-	
-	# Add the CheckBox as a child of question_options_control
+	checkbox.custom_minimum_size = Vector2(50, 0)
+	checkbox.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+	checkbox.custom_minimum_size = Vector2(250, 0)
+	checkbox.theme = checkbox_custom_theme
+
 	question_options_control.add_child(checkbox)
-	print("Added option:", option["text"])
 
 func clear_children() -> void:
 	for child in question_options_control.get_children():
