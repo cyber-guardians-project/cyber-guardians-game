bbdd340fa98e5485689e4e029a4dea2f44db24f3
Rafael Ortiz <61321805+Rafael9902@users.noreply.github.com>
2024-11-03 21:46:55 -0500

    feat: add active state to question points

--- modules/question_point/question_point.gd
+++ modules/question_point/question_point.gd
@@ -4,21 +4,31 @@ signal question_entered(question_id)
 
 @export var level: int
 @export var number: int
+@export var active: bool = true
 
 var question_layer = preload("res://modules/question_layer/question_layer.tscn")
+var question_layer_instance
 
 signal open_question(is_open: bool)
 signal select_option(option)
+signal finish_question
 
 func _on_body_entered(body: Node2D) -> void:
-	var question_instance = question_layer.instantiate()
-	var question = Utils.get_question(level, number)
-	
-	open_question.emit(true)
-	question_instance.question = question
-	question_instance.connect('select_option', _on_select_option)
-	
-	get_tree().root.add_child(question_instance)
+	if active:
+		var question_instance = question_layer.instantiate()
+		var question = Utils.get_question(level, number)
+		question_layer_instance = question_instance
+		
+		open_question.emit(true)
+		question_instance.question = question
+		question_instance.connect('select_option', _on_select_option)
+		
+		get_tree().root.add_child(question_instance)
 	
 func _on_select_option(option):
+	if option['is_correct']:
+		question_layer_instance.queue_free()
+		open_question.emit(false)
+		finish_question.emit(self)
+		
 	select_option.emit(option)
--- modules/utils/level.gd
+++ modules/utils/level.gd
@@ -2,8 +2,9 @@ extends Node2D
 
 @export var level: int
 @export var lives: int = 3
-@export var time: int = 300
+@export var time: int = 150
 @export var score: int = 0
+@export var score_to_add: int = 100
 
 @onready var player = $Player
 @onready var questions = $Questions
@@ -14,7 +15,6 @@ extends Node2D
 var hud_instance
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	hud_instance = hud.instantiate()
 	
@@ -26,7 +26,6 @@ func _ready() -> void:
 	add_child(hud_instance)
 
 		
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	tile_map_layer.visible = !is_question_active
 	player.visible = !is_question_active
@@ -52,9 +51,21 @@ func init_questions():
 		question_point.number = Utils.get_question_number(question_point.name)
 		question_point.connect('open_question', _on_open_question)
 		question_point.connect('select_option', _on_select_option)
+		question_point.connect('finish_question', _on_finish_question)
 		
 func _on_timeout():
 	print('timeout')
 	
 func _on_select_option(option):
-	pass
+	if option['is_correct']:
+		score += score_to_add
+		hud_instance.update_score(score)
+		
+		
+	if not option['is_correct']:
+		lives -= 1
+		hud_instance.update_lives(lives)
+		
+func _on_finish_question(question_point):
+	question_point.active = false
+	
