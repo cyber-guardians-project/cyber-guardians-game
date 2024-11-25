bda8045b44f4579fe3cf4735cfccb8a6ea664717
Rafael Ortiz <61321805+Rafael9902@users.noreply.github.com>
2024-11-24 22:12:56 -0500

    export

--- build/web/index.service.worker.js
+++ build/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1732472235|1255696197';
+const CACHE_VERSION = '1732504358|1805932695';
 /** @type {string} */
 const CACHE_PREFIX = 'cyber-guardians--sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
